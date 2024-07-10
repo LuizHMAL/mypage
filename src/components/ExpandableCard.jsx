c8a49ac676f0f4b62908c4898a4a3445df3d30ce
LuizHMAL <leite.luizhenrique94@gmail.com>
2024-07-10 11:26:01 -0300

    main-page

--- src/components/ExpandableCard.jsx
+++ src/components/ExpandableCard.jsx
@@ -3,11 +3,11 @@ import styles from './ExpandableCard.module.css';
 export function ExpandableCard(){
     return(
         <div className={styles.expandableCardContainer}>
-        <div className={styles.card}><img src='src/assets/images/reactLogo.svg' /></div>
-        <div className={styles.card}><img src= 'src/assets/images/jsLogo.svg'/></div>
-        <div className={styles.card}><img src= 'src/assets/images/cssLogo.svg' /></div>
-         <div className={styles.card}><img src= 'src/assets/images/htmlLogo.svg' /></div>
-         <div className={styles.card}><img src= 'src/assets/images/vitejsLogo.svg'/></div>
+        <div className={styles.card}><img src='images/reactLogo.svg' /></div>
+        <div className={styles.card}><img src= 'images/jsLogo.svg'/></div>
+        <div className={styles.card}><img src= 'images/cssLogo.svg' /></div>
+         <div className={styles.card}><img src= 'images/htmlLogo.svg' /></div>
+         <div className={styles.card}><img src= 'images/vitejsLogo.svg'/></div>
          
         </div>
         
